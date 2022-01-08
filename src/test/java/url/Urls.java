f62e69ffb991fb221b90dc74b4192fe14a8f064e
ptbuyen <ptbuyen1995@gmail.com>
2022-01-08 14:07:51 +0700

    lab17 | completed

--- src/test/java/url/Urls.java
+++ src/test/java/url/Urls.java
@@ -3,4 +3,8 @@ package url;
 public interface Urls {
     String BASE_URL = "https://the-internet.herokuapp.com";
     String DYNAMIC_CONTROL_PAGE = "/dynamic_controls";
+    String HOVERS = "/hovers";
+    String DROPDOWN = "/dropdown";
+    String IFRAME = "/iframe";
+    String JAVASCRIPT_ALERTS = "/javascript_alerts";
 }
